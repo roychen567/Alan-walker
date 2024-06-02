7f3a90770c0e18d2cf4f208048420e9e7f5bf6b9
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-06-02 18:16:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '1786837074:AAFlsPPU93FNGCbLjwhaz7uEENVejml
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/8619a6f258621134b7576.jpg https://telegra.ph/file/d8daf35960bbb4a7f8558.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/9c023dd906352100948c8.mp4')).split()
 
 # Admins, Channels & Users
 OWNER_ID = environ.get('OWNER_ID', '6350633297')
