894a50a13d5c068224fe74b7f6dc4f2a9a9fdef5
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-06-20 07:39:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -808,26 +808,26 @@ async def auto_filter(client, msg, spoll=False):
         try:
             mat = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
                                       reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(180)
-            await mat.delete()
+         #   await asyncio.sleep(180)
+        #    await mat.delete()
            # await message.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             mat = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(180)
-            await mat.delete()
+       #     await asyncio.sleep(180)
+         #   await mat.delete()
           #  await message.delete()
         except Exception as e:
             logger.exception(e)
             mat = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(180)
-            await mat.delete()
+       #     await asyncio.sleep(180)
+       #     await mat.delete()
           #  await message.delete()
     else:
         mat = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))        
-        await asyncio.sleep(180)
-        await mat.delete()
+      #  await asyncio.sleep(180)
+    #    await mat.delete()
        # await message.delete()
     if spoll:
         await msg.message.delete()
