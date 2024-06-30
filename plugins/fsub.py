60082ee9bdecfc9190c34fd8f7d6fc398491650d
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2024-06-30 16:35:28 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -26,6 +26,10 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
 
     if not AUTH_CHANNEL and not REQ_CHANNEL1 and not REQ_CHANNEL2:
         return True
+    if REQ_CHANNEL1 and if  await is_subscribed_one(bot, update):
+        return True
+    if REQ_CHANNEL2 and if  await is_subscribed_two(bot, update):
+        return True
 
     is_cb = False
     if not hasattr(update, "chat"):
