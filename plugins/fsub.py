080cafafc1d0055f9d60a1c9dbfbdadabf2793cc
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2024-08-03 20:08:30 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -72,7 +72,7 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
     except Exception as err:
         print(f"Unable to do Force Subscribe \n\nError: {err}\n\n")
         await update.reply(
-            text="Something went Wrong.",
+            text=f"Something went Wrong. {err}",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
         )
