fdb9af815d79723e76cdec61029d1018642d45b0
roychen567 <138581078+roychen567@users.noreply.github.com>
2024-12-08 18:26:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ PICS = (environ.get('PICS', 'https://graph.org/file/9c023dd906352100948c8.mp4'))
 # Admins, Channels & Users
 OWNER_ID = environ.get('OWNER_ID', '1380904444')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1380904444').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001944406326').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002203759750').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
